4fb31c9b12eb83aec19a03a58dbe2a713073f238
William Choi-Kim <will.choi.kim@gmail.com>
2025-07-22 18:29:25 -0500

    documented data

---
+++ inst/scripts/data_provenance.R
@@ -0,0 +1,8 @@
+## Source: CT-CBN C package from ETH Zurich (https://bsse.ethz.ch/cbg/software/ct-cbn.html)
+## No modifications were made to these files.
+## Formats
+# filestem.pat    - Mutational patterns (genotypes), unless N > 0
+# filestem.poset  - Event poset used if -e is not set; if -e is set, the file is used for determining the number of events as specified in the first row
+# filestem.lambda - Model parameters, if N > 0
+
+## These files are used in examples and unit tests.
